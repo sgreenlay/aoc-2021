e60878a541aefcba114c14414a322260b752bbe0
Scott Greenlay <scott@greenlay.net>
2021-11-30 21:10:45 -0800

    Reset for 2021

--- Day01.cs
+++ Day01.cs
@@ -7,23 +7,21 @@ class Day01 : IDay
 {
     int part1(IEnumerable<int> input)
     {
-        var result = input.CombinationsOfLength(2).First((pair) => pair[0] + pair[1] == 2020);
-        return result[0] * result[1];
+        return 0;
     }
 
     int part2(IEnumerable<int> input)
     {
-        var result = input.CombinationsOfLength(3).First((tuple) => tuple[0] + tuple[1] + tuple[2] == 2020);
-        return result[0] * result[1] * result[2];
+        return 0;
     }
 
     public void run()
     {
         var input = File.ReadAllText(@"input/day01.txt").SplitOnNewLine().Select(s => int.Parse(s));
 
-        var testInput = new int[]{ 1721, 979, 366, 299, 675, 1456 };
-        Debug.Assert(part1(testInput) == 514579);
-        Debug.Assert(part2(testInput) == 241861950);
+        var testInput = new int[]{ 0 };
+        Debug.Assert(part1(testInput) == 0);
+        Debug.Assert(part2(testInput) == 0);
 
         Console.WriteLine($"Part 1: {part1(input)}");
         Console.WriteLine($"Part 2: {part2(input)}");
--- Program.cs
+++ Program.cs
@@ -1,16 +1,5 @@
 ﻿
-Console.WriteLine("Advent of Code 2020");
+Console.WriteLine("Advent of Code 2021");
 
-/*
 Console.WriteLine("Day 01");
 (new Day01()).run();
-
-Console.WriteLine("Day 02");
-(new Day02()).run();
-
-Console.WriteLine("Day 03");
-(new Day03()).run();
-*/
-
-Console.WriteLine("Day 04");
-(new Day04()).run();
