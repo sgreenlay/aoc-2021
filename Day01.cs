a6f74dcd9cd3c681b5e6bca564866ddde67b8bbb
Scott Greenlay <scott@greenlay.net>
2021-11-30 22:00:41 -0800

    2021 - Day 01 Revised

--- Day01.cs
+++ Day01.cs
@@ -5,34 +5,19 @@ using Utilities;
 
 class Day01 : IDay
 {
-    int part1(int[] input)
+    int part1(IEnumerable<int> input)
     {
-        var increase = 0;
-        var prev = input[0];
-        for (var i = 1; i < input.Length; ++i)
-        {
-            if (prev < input[i])
-            {
-                increase++;
-            }
-            prev = input[i];
-        }
-        return increase;
+        return input.Window(2).Count(w => w.First() < w.Last());
     }
 
-    int part2(int[] input)
+    int part2(IEnumerable<int> input)
     {
-        int[] windows = new int[input.Length - 2];
-        for (var i = 0; i < input.Length - 2; ++i)
-        {
-            windows[i] = input[i] + input[i+1] + input[i+2];
-        }
-        return part1(windows);
+        return part1(input.Window(3).Select(w => w.Sum()));
     }
 
     public void run()
     {
-        var input = File.ReadAllText(@"input/day01.txt").SplitOnNewLine().Select(s => int.Parse(s)).ToArray();
+        var input = File.ReadAllText(@"input/day01.txt").SplitOnNewLine().Select(s => int.Parse(s));
 
         int[] testInput = { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
 
--- Utilities.cs
+++ Utilities.cs
@@ -76,6 +76,9 @@ namespace Utilities
         // Enumerate all possible combinations of a given length from a list of values
         public static IEnumerable<T[]> CombinationsOfLength<T>(this IEnumerable<T> source, int length)
         {
+            if (null == source)
+                throw new ArgumentNullException(nameof(source));
+            
             T[] data = source.ToArray();
 
             if (data.Length < length)
@@ -92,6 +95,36 @@ namespace Utilities
                 }
                 yield return (T[])result.Clone();
             }
+        }
+
+        // Enumerate all sequential windows of size from a list of values
+        public static IEnumerable<T[]> Window<T>(this IEnumerable<T> source, int size)
+        {
+            if (null == source)
+                throw new ArgumentNullException(nameof(source));
+            if (size < 1)
+                throw new ArgumentException("Size of window can't be less than 1");
+            
+            Queue<T> queue = new Queue<T>(size);
+
+            using var iter = source.GetEnumerator();
+            while (queue.Count < size && iter.MoveNext())
+            {
+                queue.Enqueue(iter.Current);
+            }
+
+            if (queue.Count < size)
+            {
+                yield break;
+            }
+
+            while (iter.MoveNext())
+            {
+                yield return queue.ToArray();
+                queue.Dequeue();
+                queue.Enqueue(iter.Current);
+            }
+            yield return queue.ToArray();
         }
     }
 }
